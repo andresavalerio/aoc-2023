5fffc5ab579ab9c356ff8e03c63c749ba26c69e0
andresavalerio <andresa.valerio@unifesp.br>
2023-12-30 16:57:38 -0300

    ex: day 1

---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2023
+
+open System
+
+module D1Trebuchet =
+    let inp = 
+        "1abc2\n\
+        pqr3stu8vwx\n\
+        a1b2c3d4e5f\n\
+        treb7uchet"
+
+    let getCalibration (s: string): int =
+        let predicate = fun (x: char) -> Char.IsDigit x
+
+        let chars = s.ToCharArray()
+
+        let first = 
+            Array.find predicate chars
+            |> Char.ToString
+
+        let last =
+            Array.find predicate (chars |> Array.rev)
+            |> Char.ToString
+        
+        first + last 
+        |> int
+
+    let total = 
+        inp.Split("\n")
+        |> Seq.map getCalibration
+        |> Seq.sum
+
+    printfn "%A" total
